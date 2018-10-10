d0ade5e4e41c0e80c959919a7a727f60f7e2edc8
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 18:04:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,9 @@
 def reverse_each_word(string)
+  # word_array = string.split
+  # reverse_array = []
+  # word_array.each {|word| reverse_array << word.reverse}
+  # return reverse_array.join(" ")
+
   word_array = string.split
-  reverse_array = []
-  word_array.each {|word| reverse_array << word.reverse}
-  return reverse_array.join(" ")
+  word_array.collect {|word| word.reverse}
 end
