276b10333a751c428ed7b3ac583a3a33151c6cea
Mike Dilley <mike.dilley@gmail.com>
2018-10-10 18:00:12 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(string)
   word_array = string.split
   reverse_word = ""
-  word_array.each {|word| reverse_word << word.reverse}
+  word_array.each {|word| reverse_word << word.reverse.join(" ")}
   return reverse_word
 end
